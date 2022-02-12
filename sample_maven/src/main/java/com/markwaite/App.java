bc9232d844b8ae27ff801032b7996c25c417f335
Mark Waite <mark.earl.waite@gmail.com>
2022-02-12 05:14:00 -0700

    Add Javadoc for main() method

--- sample_maven/src/main/java/com/markwaite/App.java
+++ sample_maven/src/main/java/com/markwaite/App.java
@@ -6,6 +6,11 @@ package com.markwaite;
  *
  */
 public class App {
+    /**
+     * Entry point into the sample program.
+     * Prints 'Hello World!' to standard output.
+     * @param args program arguments that are ignored
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
